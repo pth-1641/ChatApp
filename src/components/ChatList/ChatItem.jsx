df8981c2cb2c1561d6a2e214df33f44e88ba858a
pth-1641 <pth16042001@example.com>
2022-08-27 11:14:12 +0700

    update UI

--- src/components/ChatList/ChatItem.jsx
+++ src/components/ChatList/ChatItem.jsx
@@ -5,106 +5,98 @@ import { formatTime } from '../../constants/moment';
 import { useRoomData, useMessages } from '../../hooks';
 
 function ChatItem({ roomId }) {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const id = location.pathname.slice(1);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const id = location.pathname.slice(1);
 
-    const { uid } = useStore((state) => state.user);
+  const { uid } = useStore((state) => state.user);
 
-    const [friend, setFriend] = useState({});
-    const [sender, setSender] = useState('');
-    const roomInfo = useRoomData(roomId);
-    const messages = useMessages(roomId, 1);
+  const [friend, setFriend] = useState({});
+  const [sender, setSender] = useState('');
+  const roomInfo = useRoomData(roomId);
+  const messages = useMessages(roomId, 1);
 
-    const { chatContent, time, type } = messages[0] ?? {};
-    const { members, avatarBgColor, chatType, roomName, chatAvatar } = roomInfo;
+  const { chatContent, time, type } = messages[0] ?? {};
+  const { members, avatarBgColor, chatType, roomName, chatAvatar } = roomInfo;
 
-    useEffect(() => {
-        if (chatType === 'friend') {
-            const index = members.findIndex((mem) => mem.uid != uid);
-            setFriend(members[index]);
-        }
-    }, [roomInfo]);
+  useEffect(() => {
+    if (chatType === 'friend') {
+      const index = members.findIndex((mem) => mem.uid != uid);
+      setFriend(members[index]);
+    }
+  }, [roomInfo]);
 
-    useEffect(() => {
-        if (messages[0]?.uid === uid) {
-            setSender('You');
-        } else {
-            const memberSender = members?.find(
-                (mem) => mem.uid === messages[0]?.uid
-            );
+  useEffect(() => {
+    if (messages[0]?.uid === uid) {
+      setSender('You');
+    } else {
+      const memberSender = members?.find((mem) => mem.uid === messages[0]?.uid);
 
-            memberSender?.nickname
-                ? setSender(memberSender?.nickname)
-                : setSender(memberSender?.displayName.split(' ')[0]);
-        }
-    }, [messages, roomInfo]);
+      memberSender?.nickname
+        ? setSender(memberSender?.nickname)
+        : setSender(memberSender?.displayName.split(' ')[0]);
+    }
+  }, [messages, roomInfo]);
 
-    return (
-        <li
-            className={`h-max py-2 px-3 rounded-xl dark:hover:bg-lightDark hover:bg-gray-300 duration-200 cursor-pointer ${
-                roomId === id && 'active'
-            }`}
-            onClick={() => navigate('/' + roomId)}
+  return (
+    <li
+      className={`h-max py-2 px-3 rounded-xl dark:hover:bg-lightDark hover:bg-gray-300 duration-200 cursor-pointer ${
+        roomId === id && 'active'
+      }`}
+      onClick={() => navigate('/' + roomId)}
+    >
+      <a className='flex items-center gap-3'>
+        <div
+          className='rounded-full w-14 md:w-16 aspect-square overflow-hidden flex-center'
+          style={{ backgroundColor: avatarBgColor }}
         >
-            <a className='flex items-center gap-3'>
-                <div
-                    className='rounded-full w-14 md:w-16 aspect-square overflow-hidden flex-center'
-                    style={{ backgroundColor: avatarBgColor }}
-                >
-                    {chatType === 'friend' ? (
-                        <img
-                            src={friend.photoURL}
-                            alt=''
-                            className='w-full object-cover'
-                        />
-                    ) : chatAvatar ? (
-                        <img
-                            src={chatAvatar}
-                            alt=''
-                            className='h-full w-full object-cover'
-                        />
-                    ) : (
-                        <span className='text-white text-3xl select-none'>
-                            {roomName ? roomName[0] : ''}
-                        </span>
-                    )}
-                </div>
-                <div className='flex-between flex-1 gap-3'>
-                    <div className='w-36'>
-                        <h4 className='dark:text-white text-black font-medium'>
-                            {chatType === 'group'
-                                ? roomName
-                                : friend.nickname
-                                ? friend.nickname
-                                : friend.displayName}
-                        </h4>
-                        <p className='text-gray-600 dark:text-gray-400 text-sm truncate'>
-                            {sender}
-                            {chatContent === ''
-                                ? ' unsent a message'
-                                : type === 'images'
-                                ? ' sent an image'
-                                : type === 'videos'
-                                ? ' sent a video'
-                                    ? type === 'files'
-                                    : ' sent a file'
-                                : type === 'gifs'
-                                ? ' sent a GIF from GIPHY'
-                                : chatContent
-                                ? `: ${chatContent}`
-                                : 'No message'}
-                        </p>
-                    </div>
-                    <div className='text-gray-600 dark:text-gray-400'>
-                        <time className='text-xs'>
-                            {time?.length && formatTime(time)}
-                        </time>
-                    </div>
-                </div>
-            </a>
-        </li>
-    );
+          {chatType === 'friend' ? (
+            <img src={friend.photoURL} alt='' className='w-full object-cover' />
+          ) : chatAvatar ? (
+            <img
+              src={chatAvatar}
+              alt=''
+              className='h-full w-full object-cover'
+            />
+          ) : (
+            <span className='text-white text-3xl select-none'>
+              {roomName ? roomName[0] : ''}
+            </span>
+          )}
+        </div>
+        <div className='flex-between flex-1 gap-3'>
+          <div className='w-full md:w-36'>
+            <h4 className='dark:text-white text-black font-medium'>
+              {chatType === 'group'
+                ? roomName
+                : friend.nickname
+                ? friend.nickname
+                : friend.displayName}
+            </h4>
+            <p className='text-gray-600 dark:text-gray-400 text-sm truncate'>
+              {sender}
+              {chatContent === ''
+                ? ' unsent a message'
+                : type === 'images'
+                ? ' sent an image'
+                : type === 'videos'
+                ? ' sent a video'
+                : type === 'files'
+                ? ' sent a file'
+                : type === 'gifs'
+                ? ' sent a GIF from GIPHY'
+                : chatContent
+                ? `: ${chatContent}`
+                : 'No message'}
+            </p>
+          </div>
+          <div className='text-gray-600 dark:text-gray-400'>
+            <time className='text-xs'>{time?.length && formatTime(time)}</time>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
 }
 
 export default ChatItem;
--- src/components/ModalTemplate.jsx
+++ src/components/ModalTemplate.jsx
@@ -2,29 +2,29 @@ import { MdClose } from 'react-icons/md';
 import { useStore } from '../store';
 
 function Modal({ children, setDisplayRemoveMessage }) {
-    const setModalName = useStore((state) => state.setModalName);
+  const setModalName = useStore((state) => state.setModalName);
 
-    const handleCloseModal = () => {
-        setModalName('');
-        setDisplayRemoveMessage && setDisplayRemoveMessage(false);
-    };
+  const handleCloseModal = () => {
+    setModalName('');
+    setDisplayRemoveMessage && setDisplayRemoveMessage(false);
+  };
 
-    return (
-        <div
-            className='fixed inset-0 bg-[rgba(0,0,0,0.75)] z-20 flex-center dark:text-white p-4'
-            onClick={handleCloseModal}
-        >
-            <span className='close-modal' onClick={handleCloseModal}>
-                <MdClose />
-            </span>
-            <div
-                className='dark:bg-dark p-5 md:p-8 rounded-xl w-full max-w-[448px] bg-white'
-                onClick={(e) => e.stopPropagation()}
-            >
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className='fixed inset-0 bg-[rgba(0,0,0,0.75)] z-20 flex-center dark:text-white p-4'
+      onClick={handleCloseModal}
+    >
+      <span className='close-modal' onClick={handleCloseModal}>
+        <MdClose />
+      </span>
+      <div
+        className='dark:bg-dark p-5 md:p-8 rounded-xl w-full max-w-[448px] bg-white'
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
